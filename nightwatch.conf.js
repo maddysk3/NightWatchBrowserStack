c931c5e2d9ae3e143b37fcc08f006b4c6036db85
maddysk3 <mireshsonkable@gmail.com>
2019-08-15 20:40:10 +0530

    first commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,27 @@
+nightwatch_config = {
+  selenium : {
+    "start_process" : false,
+    "host" : "hub-cloud.browserstack.com",
+    "port" : 80
+  },
+
+  test_settings: {
+    default: {
+      desiredCapabilities: {
+        'browserstack.user': process.env.user,
+        'browserstack.key': process.env.key,
+        'browser': 'chrome',
+        'name': 'Bstack-[Nightwatch] Sample Test'
+      }
+    }
+  }
+};
+
+// Code to copy seleniumhost/port into test settings
+for(var i in nightwatch_config.test_settings){
+  var config = nightwatch_config.test_settings[i];
+  config['selenium_host'] = nightwatch_config.selenium.host;
+  config['selenium_port'] = nightwatch_config.selenium.port;
+}
+
+module.exports = nightwatch_config;
